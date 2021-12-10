fedd6f93ffe31659e454025437c888721048295f
natecheney-dev <natecheney@gmail.com>
2021-12-10 11:37:54 -0600

    project router&&model wip

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,12 @@
 // build your `Project` model here
+
+const db = require('../../data/dbConfig')
+
+const getProjects = () => {
+    return ('Joe Schmoe')
+}
+
+
+module.exports = {
+    getProjects
+}
--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,15 @@
 // build your `/api/projects` router here
+const Projects = require('./model')
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res, next) =>{
+    Projects.getProjects()
+        .then(projects => {
+            res.json(projects)
+        })
+        .catch(next)
+})
+
+
+module.exports = router;
