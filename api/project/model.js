41cfe57623d40092994170a1a5e1575b42cee00f
natecheney-dev <natecheney@gmail.com>
2021-12-10 12:35:53 -0600

    finished project model & router I think

--- api/project/model.js
+++ api/project/model.js
@@ -1,19 +1,33 @@
 // build your `Project` model here
 
+const { get } = require('superagent')
 const db = require('../../data/dbConfig')
 
-const getProjects = () => {
-    return db('projects')
+async function getProjects() {
+  const projects = await db("projects");
+  return projects.map((project) => {
+    return {
+      ...project,
+      project_completed: project.project_completed ? true : false,
+    };
+  });
 }
 
 async function getProjectById(project_id) {
     const rows = await db('projects')
         .where('project_id', project_id)
-        
+
     return rows
 }
 
+const insertProject = async (project) => {
+    const [id] = await db("projects").insert(project)
+    return db("projects").where("project_id", id).first()
+};
 
+    
 module.exports = {
-    getProjects
+    getProjects,
+    getProjectById,
+    insertProject
 }
--- api/project/router.js
+++ api/project/router.js
@@ -3,13 +3,29 @@ const express = require('express')
 const router = express.Router()
 const Projects = require('./model')
 
-router.get('/', (req, res, next) =>{
+router.get('/', (req, res, next) => {
     Projects.getProjects()
         .then(projects => {
             res.json(projects)
         })
         .catch(next)
 })
+
+router.post('/', async (req, res, next) => {
+    try {
+        const newProject = await Projects.insertProject(req.body)
+        res.status(201).json({
+            project_id: newProject.project_id,
+            project_name: newProject.project_name,
+            project_description: newProject.project_description,
+            project_completed: newProject.project_completed === 0 ? false : true
+        })
+    } catch (err) {
+        next(err)
+    }
+})
+
+
 
 
 module.exports = router;
